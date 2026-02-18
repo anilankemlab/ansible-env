b4189f6d17744400ec23d018abbacdbb5d006f9c
anilankem <anilankem@gmail.com>
2026-02-18 18:35:55 +0530

    feat: Add initial Proxmox provider configuration using `bpg/proxmox`.

---
+++ providers.tf
@@ -0,0 +1,26 @@
+
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+    }
+  }
+}
+
+variable "proxmox_api_url" {
+  type = string
+}
+
+variable "proxmox_api_token" {
+  type = string
+  sensitive = true
+}
+
+
+
+provider "proxmox" {
+  endpoint = var.proxmox_api_url
+  api_token = var.proxmox_api_token
+  insecure = true
+}
+
