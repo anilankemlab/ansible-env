bf15977817c68e75520c0daaccdba9017527aa27
anilankem <anilankem@gmail.com>
2026-02-19 08:45:51 +0530

    feat: Add GitHub Actions workflow and Terraform configuration for Proxmox VM provisioning and Ansible environment setup.

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -54,7 +54,8 @@ resource "proxmox_virtual_environment_vm" "vm_from_template" {
       username = "anilankem"
       password = "anilankem"
       keys = [
-        file("~/.ssh/id_ed25519.pub")
+        file("~/.ssh/id_ed25519.pub"),
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC3WFjIYYt/8X6DdxPwnfeq5zlMu90SfYT781koCqOZ/lRvzolcjsYpDPmZbD2Lg3WTtLHSSCBK3/Qar5LHWj9JJlLj6F2B13GF1SNUawwE3aBEbwj1eoO4fnJJtfpOuSO869qzP5//okfnSIp+htL+UHWlDOg4aclcNhDxe/C5nDwMIVeX0h8P8HfZWPTHzTrRQW6Uh0U3YqAK0+MOFrYm17F3Y6ug6oMU30E3XBCro9TIiArtXy3ht1flWRYo/XlTe78UtEkDsB/ffi9XKF3o0XAP7g+77p0wTCh9CtPl3p/umriIVgZbbMl9+UNYz4ZHRLeE0nWzALBzo0rbG9L+y4Z4ml47bzSsD1/q93A3Zj9QSfL4Xc7sRbde2sg+eZJ7SG/bBqXMcSuJ+GYy/YSm0JEO24a3RM1yYnUkbJr0bKDkyWxtziLJ0J6LkNyNxBvc5aG9w9lDUDP5NUJSL5GnBpl44ryy8DYZjoxN/id5/re0v5HFbpFXr1PumVVu3Q9UeH34ZUEGqbiBPauNJMA4r4zqcPR0ayMCO9rYP/Ni8iz93OUBVyaHc8DOL4cWgk4NTI8uV90Yg02cGRB7ZvHdiyy7u46uBn1PM+UU+dpADpg7Y6oEw7y3Dh+UMgYNXqDUfsnV+UPpFNvGdTO+FwOMb8GlRZAvIRxqo5edTD3xMQ== root@Ansible"
       ]
       sudo = true
 
